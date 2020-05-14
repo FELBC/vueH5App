8149f47e99fd56a23c27dfb96760366692f88e80
bin386@163.com <bin386@163.com>
2020-05-14 21:26:49 +0800

    vue.config.js增加devServer配置，增加mock/api.js，增加index接口数据mock，main.js增加mock开关，及axios配置，Home.vue index接口数据请求，新增Swiper.vue轮播功能

--- src/main.js
+++ src/main.js
@@ -4,11 +4,21 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios' // 将作用域对象挂载到vue实例上，方便用this调用
 import fastClick from 'fastclick' // 解决移动端(某些机型，某些浏览器上)300毫秒点击延迟问题
+import 'swiper/css/swiper.css'
 // import './assets/styles/reset.css'
 // vue.config.js配置'styles':resolve('src/assets/styles') 简化引用路径
-import 'styles/reset.css' // 
+import 'styles/reset.css' 
 import 'styles/border.css'
 import 'styles/iconfont.css'
+
+// mock开关,true使用本地mock拦截数据，false使用接口数据
+const mock = true
+if(mock){
+  require('./mock/api')
+}
+
+axios.defaults.baseURL = '/api'; // 接口代理/a/b : /api/a/b => /a/b：当前接口域名和前端域名一样可简写
+axios.defaults.timeout = 8000; // 请求超时时间，提升用户体验
 
 // 加载插件
 Vue.use(VueAxios,axios);
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" v-if="showSwiper">
+      <swiper-slide v-for="item of list" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+import { Swiper, SwiperSlide} from 'vue-awesome-swiper'
+export default {
+  name:'HomeSwiper',
+  components:{
+    Swiper,
+    SwiperSlide
+  },
+  props:{
+    list:Array
+  },
+  data() {
+    return {
+      swiperOption: {
+        autoplay:true,
+        loop:true,
+        pagination:{
+          el:'.swiper-pagination'
+        }
+      },
+    }
+  },
+  computed: {
+    showSwiper(){
+      return this.list.length
+    }
+  },
+}
+</script>
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active // >>> 样式穿透，使不受scoped限制
+    background: #fff
+    .wrapper
+      overflow:hidden
+      width:100%
+      height:0
+      padding-bottom:31.25% //撑开图片高度防止抖动
+      background:#eee
+      .swiper-img
+        width:100%
+</style>
