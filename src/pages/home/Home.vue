810b128deb6655b97a560ed6803929127370525b
bin386@163.com <bin386@163.com>
2020-05-15 14:36:07 +0800

    新增Recommend.vue首页热销推荐组件页面样式开发

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,19 +3,22 @@
     <home-header></home-header>
     <home-swiper :list="swiperList"></home-swiper>
     <home-icons :list="iconList"></home-icons>
+    <home-recommend :list="recommendList"></home-recommend>
   </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
 import axios from 'axios'
 export default {
   name:'Home',
   components:{
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend
   },
   data() {
     return {
